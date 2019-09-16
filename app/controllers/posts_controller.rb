3d5d8ab6b72d955662dd1617ba7f579834a17fde
Erin Ponsonby <erinponsonby@gmail.com>
2019-09-16 17:50:29 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @comment = Comment.create
   end
 
   def index
@@ -10,7 +9,6 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    @post.users.build
   end
 
   def create
