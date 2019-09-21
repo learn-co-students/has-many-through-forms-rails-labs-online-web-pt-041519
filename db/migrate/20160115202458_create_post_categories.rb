8aa1bad374c0e0beb64a2682b6fcb906d04dbe9c
earthctzn <earthctzn1@gmail.com>
2019-09-21 15:36:29 -0400

    Done.

--- db/migrate/20160115202458_create_post_categories.rb
+++ db/migrate/20160115202458_create_post_categories.rb
@@ -3,8 +3,6 @@ class CreatePostCategories < ActiveRecord::Migration
     create_table :post_categories do |t|
       t.belongs_to :post, index: true, foreign_key: true
       t.belongs_to :category, index: true, foreign_key: true
-      t.index ["category_id"], name: "index_post_categories_on_category_id"
-      t.index ["post_id"], name: "index_post_categories_on_post_id"
       t.timestamps null: false
     end
   end
