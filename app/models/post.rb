c0a768d0ab76f8b95e8af98d0c47d48f03cb8d5e
Meg Galvez <meg@oakmaine.com>
2019-09-30 15:35:28 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def saved_comments
+    self.comments.reject{ |c| !c.persisted?}
+  end
+
+  def commentors
+    self.comments.map { |comment| comment.user }.uniq.reject { |user| user.username.nil? }
+  end
+
 
 end
